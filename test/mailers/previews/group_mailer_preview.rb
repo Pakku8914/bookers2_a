9fff48a8392f3b6f4740080b89979fe2ba6fd0ee
rails_dev <ishii.yuuki.infratop@gmail.com>
2023-06-21 19:09:33 +0900

    9c-finish-update_gmail3

--- test/mailers/previews/group_mailer_preview.rb
+++ test/mailers/previews/group_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/group_mailer
 class GroupMailerPreview < ActionMailer::Preview
-
+  def send_mail
+    GroupMailer.with(title: 'test', content: 'test',
+                    owner_email: 'owner@owner', owner_name: 'test',
+                    user_email: 'user@user').send_mail
+  end
 end
